b800934d1b7695f853155c7bbbfb17dcf1ec6b74
Mukul Sharma <mukulsharmaudemy@gmail.com>
2024-11-29 00:27:57 +0530

    Third commit

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ clock = pygame.time.Clock()
 
 text_font = pygame.font.Font('Secrets.ttf', 100)
 
+#Player init
+player_stand = pygame.image.load('player.png').convert_alpha()
+player_stand_rect = player_stand.get_rect(center = (700,600))
+
+player = pygame.image.load('player.png').convert_alpha()
+player_rect = player.get_rect(midbottom = (200, 700))
+
+#Enemy init
+enemy = pygame.image.load('enemy.png').convert_alpha()
+enemy_rect = enemy.get_rect(midbottom = (700, 700) )
+
 game_name = text_font.render("Secret Lair", False, "Red")
 game_name_rect = game_name.get_rect(center=(700,100))
 
@@ -21,6 +32,7 @@ game_inst_rect = game_inst.get_rect(center=(700,300))
 game_active = False
 
 while True:
+    #Handling keydown events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -39,9 +51,11 @@ while True:
         pass
     
     else:
+        #Home screen
         screen.fill('Black')
         screen.blit(game_name, game_name_rect)
         screen.blit(game_inst, game_inst_rect)
+        screen.blit(player_stand, player_stand_rect)
     
     pygame.display.update()
     clock.tick(60)
