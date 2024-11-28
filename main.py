fa6e9fd197f55acea3074ec0921bde3787024f1d
Mukul Sharma <mukulsharmaudemy@gmail.com>
2024-11-29 00:18:07 +0530

    Second commit

--- main.py
+++ main.py
@@ -10,18 +10,38 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Project Secret!")
 clock = pygame.time.Clock()
 
-red = (255, 0, 0)
+text_font = pygame.font.Font('Secrets.ttf', 100)
 
-game_active = True
+game_name = text_font.render("Secret Lair", False, "Red")
+game_name_rect = game_name.get_rect(center=(700,100))
 
-while game_active:
+game_inst = text_font.render("Press Space to Start", False, "White")
+game_inst_rect = game_inst.get_rect(center=(700,300))
+
+game_active = False
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game_active = False
+            pygame.quit()
+            exit()
             
-    screen.fill(red)
+        if game_active:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    screen.fill("Green")    
+                    
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                
+    if game_active:
+        pass
     
-    pygame.display.flip()
-
-pygame.quit()
-exit()
+    else:
+        screen.fill('Black')
+        screen.blit(game_name, game_name_rect)
+        screen.blit(game_inst, game_inst_rect)
+    
+    pygame.display.update()
+    clock.tick(60)
